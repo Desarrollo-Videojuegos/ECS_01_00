9630f8f6a6387a97eb8d730879cce03014db9a63
m.gonzaleze <69689894+Maria-Gon@users.noreply.github.com>
2023-04-02 20:34:50 -0500

    fix

--- src/ecs/components/c_enemy_spawner.py
+++ src/ecs/components/c_enemy_spawner.py
@@ -5,8 +5,8 @@ from src.miembro.m_spawn_event_data import SpawnEventData
 ##.Font(None,30).render("hh", 0, (0,0,0))
 class CEnemySpawner:
     def __init__(self, level: dict) -> None:
-        self.time:float = 0
         self.spawn: list[SpawnEventData] = []
+        self.time:float = 0
         for spawn_1 in level:
             self.spawn.append(SpawnEventData(spawn_1))
 
