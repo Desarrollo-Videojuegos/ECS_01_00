4e18e9d8347577a924e9d6f2e1a53a6779be2802
m.gonzaleze <69689894+Maria-Gon@users.noreply.github.com>
2023-04-02 20:33:38 -0500

    fix

--- src/miembro/m_spawn_event_data.py
+++ src/miembro/m_spawn_event_data.py
@@ -4,6 +4,7 @@ import pygame
 
 class SpawnEventData:
     def __init__(self, data: dict) -> None:
+        self.position: pygame.Vector2 = pygame.Vector2(data['position']['x'],data['position']['y'])
         self.time: float = data['time']
         self.type: str = data['enemy_type']
-        self.position: pygame.Vector2 = pygame.Vector2(data['position']['x'],data['position']['y'])
+        
